3e9bdcc9248452de34e0e9070ea847872cc4a103
Julia Dowson <dowsonjulia@gmail.com>
2021-03-10 13:11:15 -0500

    Add project files.

---
+++ AirHockey/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace AirHockey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.winner2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(368, 23);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(61, 23);
+            this.p1ScoreLabel.TabIndex = 0;
+            this.p1ScoreLabel.Text = "0";
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(446, 23);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(62, 23);
+            this.p2ScoreLabel.TabIndex = 1;
+            this.p2ScoreLabel.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winnerLabel.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLabel.ForeColor = System.Drawing.Color.White;
+            this.winnerLabel.Location = new System.Drawing.Point(130, 157);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(223, 70);
+            this.winnerLabel.TabIndex = 2;
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winner2Label
+            // 
+            this.winner2Label.BackColor = System.Drawing.Color.Transparent;
+            this.winner2Label.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winner2Label.ForeColor = System.Drawing.Color.White;
+            this.winner2Label.Location = new System.Drawing.Point(520, 157);
+            this.winner2Label.Name = "winner2Label";
+            this.winner2Label.Size = new System.Drawing.Size(223, 70);
+            this.winner2Label.TabIndex = 3;
+            this.winner2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(830, 450);
+            this.Controls.Add(this.winner2Label);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Label winner2Label;
+    }
+}
+
---
+++ AirHockey/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AirHockey
+{
+    public partial class Form1 : Form
+
+    {
+        int paddle1X = 10;
+        int paddle1Y = 150;
+        int player1Score = 0;
+
+        int paddle2X = 600;
+        int paddle2Y = 150;
+        int player2Score = 0;
+
+        int paddleWidth = 10;
+        int paddleHeight = 60;
+        int paddleSpeed = 4;
+
+        int ballX = 295;
+        int ballY = 195;
+        int ballXSpeed = 5;
+        int ballYSpeed = 5;
+        int ballWidth = 10;
+        int ballHeight = 10;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        bool aDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+
+        Pen drawPen = new Pen(Color.White, 6);
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SoundPlayer pop = new SoundPlayer(Properties.Resources.pop);
+        SoundPlayer goal = new SoundPlayer(Properties.Resources.goal);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(drawPen, this.Width / 2 + 1, 0, this.Width / 2 + 1, this.Height);
+            e.Graphics.FillRectangle(whiteBrush, ballX, ballY, ballWidth, ballHeight);
+            e.Graphics.FillRectangle(blueBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            e.Graphics.FillRectangle(blueBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            e.Graphics.DrawLine(drawPen, 1, 140, 1, 220);
+            e.Graphics.DrawLine(drawPen, 621, 140, 621, 220);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+
+            }
+        }
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+            ballX += ballXSpeed;
+            ballY += ballYSpeed;
+
+            #region player move controls
+
+            //move player 1 
+
+            if (wDown == true && paddle1Y > 0)
+            {
+                paddle1Y -= paddleSpeed;
+            }
+
+            if (sDown == true && paddle1Y < this.Height - paddleHeight)
+            {
+                paddle1Y += paddleSpeed;
+            }
+
+            if (aDown == true && paddle1X > 0)
+            {
+                paddle1X -= paddleSpeed;
+            }
+
+            if (dDown == true && paddle1X < this.Width / 2 - paddleWidth)
+            {
+                paddle1X += paddleSpeed;
+            }
+
+
+            //move player 2 
+            if (upArrowDown == true && paddle2Y > 0)
+            {
+                paddle2Y -= paddleSpeed;
+            }
+
+            if (downArrowDown == true && paddle2Y < this.Height - paddleHeight)
+            {
+                paddle2Y += paddleSpeed;
+            }
+
+            if (leftArrowDown == true && paddle2X > this.Width / 2 + 14 - paddleWidth)
+            {
+                paddle2X -= paddleSpeed;
+            }
+
+            if (rightArrowDown == true && paddle2X < this.Width - paddleWidth)
+            {
+                paddle2X += paddleSpeed;
+            }
+            #endregion 
+            if (ballY < 0 || ballY > this.Height - ballHeight)
+            {
+                ballYSpeed *= -1;
+            }
+            if (ballX < 0 || ballX > this.Width - ballWidth)
+            {
+                ballXSpeed *= -1;
+            }
+
+            Rectangle player1Rec = new Rectangle(paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            Rectangle player2Rec = new Rectangle(paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            Rectangle player1Net = new Rectangle(1, 140, 1, 150);
+            Rectangle player2Net = new Rectangle(621, 140, 621, 150);
+            Rectangle ballRec = new Rectangle(ballX, ballY, ballWidth, ballHeight);
+
+            if (player1Rec.IntersectsWith(ballRec))
+            {
+                pop.Play();
+                ballXSpeed *= -1;
+                if (ballX <= paddle1X)
+                {
+                    ballX = paddle1X - paddleWidth - 1;
+                }
+                else
+                {
+                    ballX = paddle1X + paddleWidth + 1;
+                }
+            }
+            else if (player2Rec.IntersectsWith(ballRec))
+            {
+                pop.Play();
+                ballXSpeed *= -1;
+                if (ballX <= paddle2X)
+                {
+                    ballX = paddle2X - paddleWidth - 1;
+                }
+                else
+                {
+                    ballX = paddle2X + paddleWidth + 1;
+                }
+
+            }
+
+            if (player1Net.IntersectsWith(ballRec))
+            {
+                goal.Play();
+                player2Score++;
+                p2ScoreLabel.Text = $"{player2Score}";
+
+                paddle1X = 10;
+                paddle1Y = 150;
+
+                paddle2X = 600;
+                paddle2Y = 150;
+
+                ballX = 295;
+                ballY = 195;
+                ballXSpeed = 4;
+                ballYSpeed = 4;
+                ballWidth = 10;
+                ballHeight = 10;
+
+            }
+            if (player2Net.IntersectsWith(ballRec))
+            {
+                goal.Play();
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                paddle1X = 10;
+                paddle1Y = 150;
+
+                paddle2X = 600;
+                paddle2Y = 150;
+
+                ballX = 295;
+                ballY = 195;
+                ballXSpeed = 4;
+                ballYSpeed = 4;
+                ballWidth = 10;
+                ballHeight = 10;
+
+            }
+            if (player1Score == 3)
+            {
+                gameTimer.Enabled = false;
+                winnerLabel.Text = "Player 1 Wins!";
+            }
+            else if (player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+                winner2Label.Text = "Player 2 Wins!";
+            }
+
+            Refresh();
+        }
+    }
+}
